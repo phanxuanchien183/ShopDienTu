7c4062c8920bc21349af370f3c0a39fc628cc533
phanxuanchien183 <phanxuanchien183@gmail.com>
2021-01-02 17:45:50 +0700

    update data in popup modal

--- resources/views/page/trangchu.blade.php
+++ resources/views/page/trangchu.blade.php
@@ -128,7 +128,7 @@
                                                             <li class="add-cart active"><a href="{{route('themgiohang',$sp->id)}}">Add to cart</a></li>
                                                             <li><a class="links-details" href="wishlist.html"><i class="fa fa-heart-o"></i></a></li>
                                                             {{-- <li><a href="{{route('chitietsanphampopup',$sp->id)}}" title="quick view" class="quick-view-btn" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-eye"></i></a></li> --}}
-                                                            <li><a href="#" title="quick view" class="quick-view-btn" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-eye"></i></a></li>
+                                                            <li><a href="#" title="quick view" data-name="{{$sp}}" class="quick-view-btn" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-eye"></i></a></li>
                                                         </ul>
                                                     </div>
                                                 </div>
@@ -136,7 +136,13 @@
                                             <!-- single-product-wrap end -->
                                         </div>
                                     @endforeach
-                                    
+                                    <script>
+                                        $(document).on('ready',function(){
+                                            $(".quick-view-btn").on('click',function(){
+                                                $("#sp-chien-name").text($(this).data('name')->name);
+                                            })
+                                        })
+                                    </script>
                                     
                                 </div>
                             </div>
@@ -1316,118 +1322,5 @@
                     </div>
                 </div>
             </section>
-            {{-- <div class="modal fade modal-wrapper" id="editModal" >
-                <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                        <form action="{{ action('/employee')}}" method="POST" id="editform">
-                            {{csrf_field()}}
-                            {{method_field('PUT')}}
-                            <div class="modal-body">
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                                <div class="modal-inner-area row">
-                                    <div class="col-lg-5 col-md-6 col-sm-6">
-                                    <!-- Product Details Left -->
-                                        <div class="product-details-left">
-                                            <div class="product-details-images slider-navigation-1">
-                                                <div class="lg-image">
-                                                    <img src="uiStore/images/product/large-size/1.jpg" alt="product image">
-                                                </div>
-                                                <div class="lg-image">
-                                                    <img src="uiStore/images/product/large-size/2.jpg" alt="product image">
-                                                </div>
-                                                <div class="lg-image">
-                                                    <img src="uiStore/images/product/large-size/3.jpg" alt="product image">
-                                                </div>
-                                                <div class="lg-image">
-                                                    <img src="uiStore/images/product/large-size/4.jpg" alt="product image">
-                                                </div>
-                                                <div class="lg-image">
-                                                    <img src="uiStore/images/product/large-size/5.jpg" alt="product image">
-                                                </div>
-                                                <div class="lg-image">
-                                                    <img src="uiStore/images/product/large-size/6.jpg" alt="product image">
-                                                </div>
-                                            </div>
-                                            <div class="product-details-thumbs slider-thumbs-1">                                        
-                                                <div class="sm-image"><img src="uiStore/images/product/small-size/1.jpg" alt="product image thumb"></div>
-                                                <div class="sm-image"><img src="uiStore/images/product/small-size/2.jpg" alt="product image thumb"></div>
-                                                <div class="sm-image"><img src="uiStore/images/product/small-size/3.jpg" alt="product image thumb"></div>
-                                                <div class="sm-image"><img src="uiStore/images/product/small-size/4.jpg" alt="product image thumb"></div>
-                                                <div class="sm-image"><img src="uiStore/images/product/small-size/5.jpg" alt="product image thumb"></div>
-                                                <div class="sm-image"><img src="uiStore/images/product/small-size/6.jpg" alt="product image thumb"></div>
-                                            </div>
-                                        </div>
-                                        <!--// Product Details Left -->
-                                    </div>
-
-                                    <div class="col-lg-7 col-md-6 col-sm-6">
-                                        <div class="product-details-view-content pt-60">
-                                            <div class="product-info">
-                                                <h2>{{$sanpham->name}}</h2>
-                                                <span class="product-details-ref">Reference: demo_15</span>
-                                                <div class="rating-box pt-20">
-                                                    <ul class="rating rating-with-review-item">
-                                                        <li><i class="fa fa-star-o"></i></li>
-                                                        <li><i class="fa fa-star-o"></i></li>
-                                                        <li><i class="fa fa-star-o"></i></li>
-                                                        <li class="no-star"><i class="fa fa-star-o"></i></li>
-                                                        <li class="no-star"><i class="fa fa-star-o"></i></li>
-                                                        <li class="review-item"><a href="#">Read Review</a></li>
-                                                        <li class="review-item"><a href="#">Write Review</a></li>
-                                                    </ul>
-                                                </div>
-                                                <div class="price-box pt-20">
-                                                    <span class="new-price new-price-2">$57.98</span>
-                                                </div>
-                                                <div class="product-desc">
-                                                    <p>
-                                                        <span>100% cotton double printed dress. Black and white striped top and orange high waisted skater skirt bottom. Lorem ipsum dolor sit amet, consectetur adipisicing elit. quibusdam corporis, earum facilis et nostrum dolorum accusamus similique eveniet quia pariatur.
-                                                        </span>
-                                                    </p>
-                                                </div>
-                                                <div class="product-variants">
-                                                    <div class="produt-variants-size">
-                                                        <label>Dimension</label>
-                                                        <select class="nice-select">
-                                                            <option value="1" title="S" selected="selected">40x60cm</option>
-                                                            <option value="2" title="M">60x90cm</option>
-                                                            <option value="3" title="L">80x120cm</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                                <div class="single-add-to-cart">
-                                                    <form action="#" class="cart-quantity">
-                                                        <div class="quantity">
-                                                            <label>Quantity</label>
-                                                            <div class="cart-plus-minus">
-                                                                <input class="cart-plus-minus-box" value="1" type="text">
-                                                                <div class="dec qtybutton"><i class="fa fa-angle-down"></i></div>
-                                                                <div class="inc qtybutton"><i class="fa fa-angle-up"></i></div>
-                                                            </div>
-                                                        </div>
-                                                        <button class="add-to-cart" type="submit">Add to cart</button>
-                                                    </form>
-                                                </div>
-                                                <div class="product-additional-info pt-25">
-                                                    <a class="wishlist-btn" href="wishlist.html"><i class="fa fa-heart-o"></i>Add to wishlist</a>
-                                                    <div class="product-social-sharing pt-25">
-                                                        <ul>
-                                                            <li class="facebook"><a href="#"><i class="fa fa-facebook"></i>Facebook</a></li>
-                                                            <li class="twitter"><a href="#"><i class="fa fa-twitter"></i>Twitter</a></li>
-                                                            <li class="google-plus"><a href="#"><i class="fa fa-google-plus"></i>Google +</a></li>
-                                                            <li class="instagram"><a href="#"><i class="fa fa-instagram"></i>Instagram</a></li>
-                                                        </ul>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>  --}}
+          
 @endsection
